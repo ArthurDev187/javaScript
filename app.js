03a9d781c3a5589f8d92b971df037c2db43d4307
Arthur Rodrigues <arthurr.rodrigues187@gmail.com>
2025-05-24 23:42:29 +0800

    Progressing in javascript

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 alert('Boas vindas ao jogo do número secreto.');
 let numeroSecreto = 2;
+let chute = prompt('Digite um número entre 1 e 10');
 
-prompt('Digite um número entre 1 e 10: ');
+if (numeroSecreto == chute) {
+    console.log("Isso aí, você acertou o número secreto. (2)");
+}
