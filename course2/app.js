4cab847e92df814b768a5bc9ba9669da68293683
Arthur Rodrigues da Silva <arthurr.rodrigues187@gmail.com>
2025-06-16 20:53:23 +0800

    improving the game

--- course2/app.js
+++ course2/app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroSecreto();
+let tentativas = 1;
 
 function exibirTextoNaTela (tag, texto) {
     let campo = document.querySelector(tag);
@@ -10,17 +11,26 @@ exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    let mensagem = `Voce descobriu o numero secreto com ${tentativas} ${palavraTentativa}`;
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!!!');
-        exibirTextoNaTela('p', 'Voce descobriu o numero secreto');
+        exibirTextoNaTela('p', mensagem);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     } else if (chute > numeroSecreto) {
         exibirTextoNaTela('p', 'O numero secreto e menor');
     } else {
-        exibirTextoNaTela('p', 'O numero secreto e maior')
+        exibirTextoNaTela('p', 'O numero secreto e maior');
     }
+    limparCampo();
+    tentativas++;
 }
 
 function gerarNumeroSecreto() {
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = ''
 }
